1c236d25d240e1f157b2bed5190d1d57a520e1ba
dab7942 <35974884+dab7942@users.noreply.github.com>
2021-04-01 07:31:48 -0700

    Can now prove it can work (in theory)

--- latex.py
+++ latex.py
@@ -1,1 +1,17 @@
 # Commented line
+import os
+
+from cuckoo.common.abstracts import Report
+from cuckoo.common.exceptions import CuckooReportError
+
+class Latex(Report):
+     # This does things
+     # Attempt to do a thing
+     def run(self, results):
+          try:
+               alt_file_path = os.path.join(self.reports_path, "latex.txt")
+               with open(alt_file_path, "w") as test_report:
+                    test_report.write("I AM CREATING A FILE!!!")
+                    test_report.close()
+          except (TypeError, IOError) as e:
+               raise CuckooReportError("Didn't generate duncan report: %s" % e)     
