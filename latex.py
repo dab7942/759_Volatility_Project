80210b0c5bf5c0ed3ed46961bc811c749ee37f98
dab7942 <35974884+dab7942@users.noreply.github.com>
2021-04-14 07:16:58 -0700

    Significant Updates (4/13)

--- latex.py
+++ latex.py
@@ -13,23 +13,37 @@ from cuckoo.common.exceptions import CuckooReportError
 # Start working on cool looking process tree
 # File access timeline
 
+# Notes for making process tree
+# After defining a node, add "[level distance= ...]" on next line
+# Use width("text") to determine distance
+# Can regrab process name for this
+
+# Also need "text centered" in tikzpicture
+# And // in the text to split it across lines
+
 class Latex(Report):
+
+#     new_samplename = ""
+
+     def fix_underscores(self, broke):
+          underscore = broke.find("_")
+          fixed = ""
+          if underscore != -1:
+               splits = broke.split("_")
+               for bit in splits:
+                    fixed += bit
+                    fixed += "\_"
+               fixed = fixed[0:-2]
+          else:
+               fixed = broke
+          return fixed
+
 
      def add_title(self, results):
           title = ""
           try:
-               samplename = results['target']['file']['name']
-               underscore = samplename.find("_")
-               new_samplename = ""
-               if underscore != -1:
-                    splits = samplename.split("_")
-                    for bit in splits:
-                         new_samplename += bit
-                         new_samplename += "\_"
-                    new_samplename = new_samplename[0:-2]
-               else:
-                    new_samplename = samplename
-               title += "\\title{Latex Report for " + new_samplename + "}\n"
+               sample = self.fix_underscores(results['target']['file']['name'])
+               title += "\\title{Latex Report for " + str(sample) + "}\n"
                title += "\\author{Cuckoo Sandbox}\n"
                today = date.today().strftime("%B %d, %Y")
                title += "\date{" + today + "}\n"
@@ -42,7 +56,6 @@ class Latex(Report):
           tab = "\indent "
           sig_notes = ""
           try:
-#               sig_notes = ""
                sig_notes += "\section{Signatures}\n"
                sig_notes += "\label{Signatures}\n"
                for sig in results['signatures']:
@@ -59,7 +72,8 @@ class Latex(Report):
                     if calls != {}:
                          sig_notes += tab + "Relevant API Calls\n\n"
                          for call, count in calls.items():
-                              sig_notes += tab + tab + call + " was called "
+                              call2 = self.fix_underscores(call)
+                              sig_notes += tab + tab + call2 + " was called "
                               sig_notes += str(count) + " times\n\n"
                     if refs != "":
                          sig_notes += tab + "For further info\n\n"
@@ -76,67 +90,118 @@ class Latex(Report):
                return sig_notes
 
      def rec_tree(self, node):
-          notes_list = []
+          rec_notes = ""
           try:
-               rec_notes = ""
-#               notes_list = []
-               pid = node['pid']
-               kids = []
-               rec_notes += "Process " + str(pid) + " had kids"
+               rec_notes += "\nchild{node[roundnode]{" + str(node['pid']) + "}"
                for kid in node['children']:
-                    rec_notes += " " + str(kid['pid'])
-                    kid_info = self.rec_tree(kid)
-                    notes_list.extend(kid_info)
-               rec_notes += "\n\n"
-               notes_list.append(rec_notes)
+                    rec_notes += rec_tree(kid)
+               rec_notes += "}"
           except:
                raise CuckooReportError("Rec tree issue: ", sys.exec_info()[0])
           finally:
-               return notes_list
+               return rec_notes
           
 
      def tree(self, results):
           tree_notes = ""
           try:
-#               tree_notes = ""
-               tree_list = []
+               tree_notes += "\\newpage"
                tree_notes += "\section{Process Tree}\n"
                tree_notes += "\label{Process Tree}\n"
                proc_tree = results['behavior']['processtree']
                for node in proc_tree:
-                    tree_list.extend(self.rec_tree(node))
-               for item in tree_list:
-                    tree_notes += item
-#                    pid = node['pid']
-#                    kids = []
-#                    tree_notes += "Process " + str(pid) + " had kids"
-#                    for kid in node['children']:
-#                         tree_notes += " " + str(kid['pid'])
-#                    tree_notes += "\n\n"
+                    tree_notes += "\\begin{tikzpicture}"
+                    style = "{circle,draw=black,fill=white,minimum size=4mm}"
+                    proc_name = self.fix_underscores(node['process_name'])
+                    blurb = str(node['pid']) + "\n" + str(proc_name)
+                    tree_notes += "[roundnode/.style=" + style + "]\n"
+                    tree_notes += "\\node[roundnode]{" + blurb + "}\n"
+                    tree_notes += "[level distance=width(\"" + blurb + "\")]"
+                    for kid in node['children']:
+                         tree_notes += self.rec_tree(kid)
+                    tree_notes += ";\n"
+                    tree_notes += "\end{tikzpicture}\n"
           except:
                raise CuckooReportError("Other sig issue: ", sys.exec_info()[0])
           finally:
                return tree_notes
 
+     def pe_sec(self, results):
+          pe_notes = ""
+          try:
+               pe_notes += "\section{PE}\n"
+               pe_notes += "\label{PE}\n"
+               pe_notes += "\\begin{tikzpicture}]\n"
+               total_size = 0.0
+               base_shape = "\\filldraw[fill=red!{:.2f}!green, draw=black]"
+               base_shape += " (0,{:.2f}) rectangle (11,{:.2f});\n"
+               base_text = "\\node[anchor=west] at (1,{:.2f}) {};"
+               secs = results['static']['pe_sections']
+               for sec in secs:
+                    size = int(sec['size_of_data'], 16)
+                    total_size += float(size)
+               top = 19.00
+               deficit = 0
+               num_sec = len(secs)
+               count = 1
+               for sec in secs:
+                    size = int(sec['size_of_data'], 16)
+                    size = float(size)
+                    scale = size/total_size*19.0
+                    height = 0
+                    if scale < 1.00:
+                         deficit += (1.00 - scale)
+                         height = 1.00
+                    elif scale > deficit + 2.00:
+                         height = scale - deficit
+                         deficit = 0.00
+                    else:
+                         height = scale
+                    shade = float(100*count/num_sec)
+                    pe_notes += base_shape.format(shade, top, top-height)
+                    count += 1
+                    core_text = "{" + sec['name'] + "}"
+                    pe_notes += base_text.format(top-0.5, core_text) + "\n"
+                    top -= height
+               pe_notes += "\end{tikzpicture}"
+          except:
+               raise CuckooReportError("Other sig issue: ", sys.exec_info()[0])
+          finally:
+               return pe_notes
+
      # This does things
      # Attempt to do a thing
      def run(self, results):
           try:
-               whole_document = ""
-               whole_document += "\documentclass{report}\n"
-               whole_document += self.add_title(results)
-               whole_document += "\\begin{document}\n"
-               whole_document += "\maketitle\n"
-               whole_document += "\section{Introduction}\n"
-               whole_document += "\label{Introduction}\n"
-               whole_document += "There will be something here eventually\n"
-               whole_document += self.sig_analysis(results)
-               whole_document += self.tree(results)
+               whole_doc = ""
+               whole_doc += "\documentclass{report}\n"
+               whole_doc += "\usepackage{tikz}\n"
+               whole_doc += "\usepackage[a4paper, margin=1in]{geometry}\n"
+               whole_doc += self.add_title(results)
+               sample = self.fix_underscores(results['target']['file']['name'])
+               whole_doc += "\\begin{document}\n"
+               whole_doc += "\maketitle\n"
+               whole_doc += "\section{Introduction}\n"
+               whole_doc += "\label{Introduction}\n"
+               whole_doc += "This report contains a summary of various "
+               whole_doc += "activities and artifacts documented by the "
+               whole_doc += "cuckoo sandbox report. This report came from "
+               whole_doc += " executing a file called "
+               whole_doc += str(sample) + " on a "
+               whole_doc += results['info']['machine']['manager'] + " VM. "
+               whole_doc += "It was given a score of "
+               whole_doc += str(results['info']['score']) + ". "
+               whole_doc += "The sections are: "
+               whole_doc += "signatures, process tree .\n"
+#               whole_doc += "There will be something here eventually\n"
+               whole_doc += self.sig_analysis(results)
+               whole_doc += self.tree(results)
+               whole_doc += self.pe_sec(results)
 # This is the very last thing to add
-               whole_document += "\end{document}"
+               whole_doc += "\end{document}"
                alt_file_path = os.path.join(self.reports_path, "latex.txt")
                with open(alt_file_path, "w") as test_report:
-                    test_report.write(whole_document)
+                    test_report.write(whole_doc)
                     test_report.close()
           except (TypeError, IOError) as e:
                raise CuckooReportError("Didn't generate duncan report: %s" % e)     
