c720c23f3437909cd6a46889038ac93afa133c95
dab7942 <35974884+dab7942@users.noreply.github.com>
2021-04-18 16:39:45 -0700

    Added API stat stuff

--- latex.py
+++ latex.py
@@ -7,23 +7,23 @@ from cuckoo.common.exceptions import CuckooReportError
 
 #global tab = "\indent"
 
+sample = ""
+
 # To do
-# Add slashes \ to override underscores _ in API names
-# Add families check along with references check (Works the same way)
-# Start working on cool looking process tree
 # File access timeline
+  # Found the right place to find this info. Start on code tomorrow.
+# Maybe improve process tree somehow?
+# Gotta find something else
 
-# Notes for making process tree
-# After defining a node, add "[level distance= ...]" on next line
-# Use width("text") to determine distance
-# Can regrab process name for this
 
-# Also need "text centered" in tikzpicture
-# And // in the text to split it across lines
+# THESE THINGS ARE DONE
+# Improve API call graphs by fixing alignment & adding Title/Header
+  # This should be solved
+# Added graph for overall stats
+# Consider adding additional function for graph gereration to offload code
+  # Actually this isn't possible since I need to build the overall stats
 
 class Latex(Report):
-
-#     new_samplename = ""
 
      def fix_underscores(self, broke):
           underscore = broke.find("_")
@@ -38,11 +38,10 @@ class Latex(Report):
                fixed = broke
           return fixed
 
-
      def add_title(self, results):
           title = ""
           try:
-               sample = self.fix_underscores(results['target']['file']['name'])
+               global sample
                title += "\\title{Latex Report for " + str(sample) + "}\n"
                title += "\\author{Cuckoo Sandbox}\n"
                today = date.today().strftime("%B %d, %Y")
@@ -55,6 +54,7 @@ class Latex(Report):
      def sig_analysis(self, results):
           tab = "\indent "
           sig_notes = ""
+          apis = []
           try:
                sig_notes += "\section{Signatures}\n"
                sig_notes += "\label{Signatures}\n"
@@ -63,12 +63,16 @@ class Latex(Report):
                     sig_notes += "Description: " + sig['description'] + "\n\n"
                     calls = {}
                     refs = ""
+                    fams = ""
                     for mark in sig['marks']:
                          if mark['type'] == "call":
                               api = mark['call']['api']
                               calls[api] = calls.get(api, 0) + 1
+                              apis.append(api)
                     for ref in sig['references']:
                          refs += tab + tab + ref + "\n\n"
+                    for fam in sig['families']:
+                         fams += tab + tab + fam + "\n\n"
                     if calls != {}:
                          sig_notes += tab + "Relevant API Calls\n\n"
                          for call, count in calls.items():
@@ -78,16 +82,15 @@ class Latex(Report):
                     if refs != "":
                          sig_notes += tab + "For further info\n\n"
                          sig_notes += refs
-# Leaving this part out until I learn what families actually looks like
-#               if len(sig['families']) > 0:
-#                    sig_notes += "\t Families\n"
-#                    for fam in sig['families']:
+                    if fams != "":
+                         sig_notes += tab + "Families with this signature\n\n"
+                         sig_notes += fams
           except (TypeError, IOError) as e:
                raise CuckooReportError("Issue with sig analysis: %s" % e)
           except:
                raise CuckooReportError("Other sig issue: ", sys.exec_info()[0])
           finally:
-               return sig_notes
+               return (sig_notes, apis)
 
      def rec_tree(self, node):
           rec_notes = ""
@@ -96,8 +99,6 @@ class Latex(Report):
                blurb = str(node['pid']) + r"\\" + proc_name
                rec_notes += "\nchild{node[roundnode]{" + blurb + "}"
                rec_notes += "[level distance=width(\"" + blurb + "\")]"
-#               proc_name = self.fix_underscores(node['process_name'])
-#               blurb = str(node['pid']) + "\n" + str(proc_name)
                for kid in node['children']:
                     rec_notes += self.rec_tree(kid)
                rec_notes += "}"
@@ -116,12 +117,23 @@ class Latex(Report):
                proc_tree = results['behavior']['processtree']
                for node in proc_tree:
                     tree_notes += "\\begin{tikzpicture}"
-                    style = "{circle,draw=black,fill=white,minimum size=4mm}"
-                    node_def = "roundnode/.style="
-                    node_def += style
+                    l1 = "level 1/.style={sibling distance = 30mm},"
+                    grn = "{circle,draw=black,fill=green!50,minimum size=4mm},"
+                    red = "{circle,draw=black,fill=red!50,minimum size=4mm},"
+                    ng = "{circle,draw=black,fill=orange!50,minimum size=4mm},"
                     proc_name = self.fix_underscores(node['process_name'])
+                    if(proc_name[-4:] == ".exe"):
+                         proc_name = proc_name[0:-4]
+                    global sample
+                    node_def = ""
+                    if(proc_name == sample):
+                         node_def = "roundnode/.style=" + red
+                    elif(proc_name == "lsass"):
+                         node_def = "roundnode/.style=" + grn
+                    else:
+                         node_def = "roundnode/.style=" + ng
                     blurb = str(node['pid']) + r"\\" + proc_name
-                    tree_notes += "[" + node_def + ", align=center]\n"
+                    tree_notes += "[" + l1 + node_def + "align=center]\n"
                     tree_notes += "\\node[roundnode]{" + blurb + "}\n"
                     tree_notes += "[level distance=width(\"" + blurb + "\")]"
                     for kid in node['children']:
@@ -176,16 +188,95 @@ class Latex(Report):
           finally:
                return pe_notes
 
+     def apicalls(self, results, badapis):
+          call_notes = ""
+          try:
+               call_notes += "\section{API Calls}\n"
+               call_notes += "\label{API Calls}\n"
+               stats = results['behavior']['apistats']
+               totals = {}
+               for proc in stats:
+                    graph = "\\begin{tikzpicture}\n\\begin{axis}\n"
+                    graph += "[ybar, enlargelimits=0.15, ylabel={Count}, "
+                    graph += "title={Most common calls for " + str(proc) + "}"
+                    graph += ", nodes near coords, "
+                    graph += "nodes near coords align={vertical}, "
+                    graph += "xtick=data, xticklabel style={rotate=-90},"
+                    coords = "symbolic x coords={"
+                    plots = "\\addplot coordinates {"
+                    redplots = "\\addplot[ybar,fill=red] coordinates {"
+                    calls = []
+                    counts = []
+                    for key in stats[proc]:
+                         calls.append(key)
+                         counts.append(stats[proc][key])
+                         totals[key] = totals.get(key, 0) + stats[proc][key]
+                    for i in range(5):
+                          index = counts.index(max(counts))
+                          call = calls[index]
+                          coords += call + ", "
+                          if call in badapis:
+                               redplots += "(" + call + ", "
+                               redplots += str(counts[index]) + ") "
+                          else:
+                               plots += "(" + call + ", "
+                               plots += str(counts[index]) + ") "
+                          calls = calls[:index] + calls[index+1:]
+                          counts = counts[:index] + counts[index+1:]
+                    coords += "}]\n"
+                    plots += "};\n"
+                    redplots += "};\n"
+                    graph += coords + plots + redplots
+                    graph += "\end{axis}\n\end{tikzpicture}\n"
+                    call_notes += graph
+               graph = "\\begin{tikzpicture}\n\\begin{axis}\n"
+               graph += "[ybar, enlargelimits=0.15, ylabel={Count}, "
+               graph += "title={Most common calls overall}"
+               graph += ", nodes near coords, "
+               graph += "nodes near coords align={vertical}, "
+               graph += "xtick=data, xticklabel style={rotate=-90},"
+               coords = "symbolic x coords={"
+               plots = "\\addplot coordinates {"
+               redplots = "\\addplot[ybar,fill=red] coordinates {"
+               calls = []
+               counts = []
+               for key in totals:
+                    calls.append(key)
+                    counts.append(totals[key])
+               for i in range(5):
+                     index = counts.index(max(counts))
+                     coords += calls[index] + ", "
+                     if calls[index] in badapis:
+                          redplots += "(" + calls[index] + ", "
+                          redplots += str(counts[index]) + ") "
+                     else:
+                          plots += "(" + calls[index] + ", "
+                          plots += str(counts[index]) + ") "
+                     calls = calls[:index] + calls[index+1:]
+                     counts = counts[:index] + counts[index+1:]
+               coords += "}]\n"
+               plots += "};\n"
+               redplots += "};\n"
+               graph += coords + plots + redplots
+               graph += "\end{axis}\n\end{tikzpicture}\n"
+               call_notes += graph
+          except:
+               raise CuckooReportError("Other sig issue: ", sys.exec_info()[0])
+          finally:
+               return call_notes
+
      # This does things
      # Attempt to do a thing
      def run(self, results):
           try:
+               global sample
+               sample = self.fix_underscores(results['target']['file']['name'])
                whole_doc = ""
                whole_doc += "\documentclass{report}\n"
                whole_doc += "\usepackage{tikz}\n"
                whole_doc += "\usepackage[a4paper, margin=1in]{geometry}\n"
+               whole_doc += "\usepackage{pgfplots}\n"
                whole_doc += self.add_title(results)
-               sample = self.fix_underscores(results['target']['file']['name'])
                whole_doc += "\\begin{document}\n"
                whole_doc += "\maketitle\n"
                whole_doc += "\section{Introduction}\n"
@@ -200,10 +291,12 @@ class Latex(Report):
                whole_doc += str(results['info']['score']) + ". "
                whole_doc += "The sections are: "
                whole_doc += "signatures, process tree .\n"
-#               whole_doc += "There will be something here eventually\n"
-               whole_doc += self.sig_analysis(results)
+               (sigs, apis) = self.sig_analysis(results)
+               whole_doc += sigs
+#               whole_doc += self.sig_analysis(results)
                whole_doc += self.tree(results)
                whole_doc += self.pe_sec(results)
+               whole_doc += self.apicalls(results, apis)
 # This is the very last thing to add
                whole_doc += "\end{document}"
                alt_file_path = os.path.join(self.reports_path, "latex.txt")
